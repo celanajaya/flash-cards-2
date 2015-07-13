254a198bb79df80d98548489aecfe62624d000cd
celanajaya <petermsteele@gmail.com>
2015-07-13 14:36:31 -0400

    getting ready for day 4

--- public/js/directives/flashCard.js
+++ public/js/directives/flashCard.js
@@ -1,8 +1,10 @@
+//directives are camel case here, but dash case in HTML
 app.directive('flashCard', function(ScoreFactory){
 	return {
 		restrict: 'E',
+		card: '=',
+			//or theCard: '=card' also maps the variable name
 		templateUrl: "/js/directives/flashCard.html",
-		card: "=",
 		link: function(scope, element, attribute){
 			scope.answerQuestion = function (answer, flashCard) {
 				if (!flashCard.answered) {
--- public/js/directives/loader.js
+++ public/js/directives/loader.js
@@ -1,6 +1,9 @@
 app.directive('loader', function(){
 	return {
 		restrict: 'E',
+		scope: true,
 		templateUrl: "/js/directives/loader.html"
 	};
 });
+
+//templateUrl makes an AJAX request to the address specified.
